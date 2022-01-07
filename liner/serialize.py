2b4d08d3f2cd9ae9dcbee09d69b9b04046fef1f5
Khoi Pham <pckhoi@gmail.com>
2022-01-07 14:51:33 +0700

    test: dump and load yaml

--- liner/serialize.py
+++ liner/serialize.py
@@ -3,53 +3,67 @@ import typing
 
 import attr
 import yaml
+from yaml.dumper import Dumper
 
 
 def to_camel_case(s):
-    components = s.split('_')
-    return components[0] + ''.join(x.title() for x in components[1:])
+    components = s.split("_")
+    return components[0] + "".join(x.title() for x in components[1:])
 
 
-word_break_pattern = re.compile(r'([a-z])([A-Z])')
+word_break_pattern = re.compile(r"([a-z])([A-Z])")
 
 
 def to_snake_case(s):
-    return word_break_pattern.sub(r'\1_\2', s).lower()
+    return word_break_pattern.sub(r"\1_\2", s).lower()
 
 
-class _YAMLDumper(yaml.Dumper):
-    def represent_object(self, data):
-        if attr.has(data.__class__):
-            fields = attr.fields(data.__class__)
-            return self.represent_dict({
-                to_camel_case(field.name): getattr(data, field.name)
-                for field in fields if getattr(data, field.name) is not None
-            })
-        return super().represent_object(data)
+def noop(self, *args, **kw):
+    pass
+
+
+yaml.emitter.Emitter.process_tag = noop
+
+
+def _object_as_dict(obj):
+    if attr.has(obj.__class__):
+        fields = attr.fields(obj.__class__)
+        return {
+            to_camel_case(field.name): _object_as_dict(getattr(obj, field.name))
+            for field in fields
+            if getattr(obj, field.name, None) is not None
+        }
+    return obj
+
+
+def represent_attr_object(dumper, data):
+    if attr.has(data.__class__):
+        return dumper.represent_dict(_object_as_dict(data))
+    return dumper.represent_object(data)
+
+
+yaml.add_multi_representer(object, represent_attr_object)
 
 
 def yaml_dump(data, **kwargs):
-    return yaml.dump(data, Dumper=_YAMLDumper, **kwargs)
+    return yaml.dump(data, Dumper=Dumper, **kwargs)
 
 
 def yaml_load(s, serializer_cls):
-    data = yaml.load(s)
+    data = yaml.load(s, Loader=yaml.Loader)
     return _deserialize(data, serializer_cls)
 
 
 def _deserialize(data, serializer_cls):
     kwargs = dict()
     fields_dict = attr.fields_dict(serializer_cls)
-    data = [
-        (to_snake_case(k), v) for k, v in data.items()
-    ]
-    data_stack = [
-        (kwargs, k, fields_dict[k], v)
-        for k, v in data if k != 'meta'
-    ]
+    data = [(to_snake_case(k), v) for k, v in data.items()]
+    data_stack = [(kwargs, k, fields_dict[k], v) for k, v in data if k != "meta"]
     while len(data_stack) > 0:
         parent, name, field, value = data_stack.pop()
-        if type(field.type) is type:
+        if value is None:
+            parent[name] = value
+        elif type(field.type) is type:
             if attr.has(field.type):
                 parent[name] = _deserialize(value, field.type)
             else:
@@ -58,26 +72,22 @@ def _deserialize(data, serializer_cls):
             el_cls = field.type.__args__[0]
             if type(value) is not list:
                 raise TypeError(
-                    'keyword argument "%s" should be a list of %s' % (name, el_cls))
+                    'keyword argument "%s" should be a list of %s' % (name, el_cls)
+                )
             if attr.has(el_cls):
-                parent[name] = [
-                    _deserialize(e, el_cls) for e in value
-                ]
+                parent[name] = [_deserialize(e, el_cls) for e in value]
             else:
                 parent[name] = value
         elif field.type.__origin__ is dict or field.type.__origin__ is typing.Dict:
             el_cls = field.type.__args__[1]
             if type(value) is not dict:
                 raise TypeError(
-                    'keyword argument "%s" should be a dict of %s' % (
-                        name, el_cls)
+                    'keyword argument "%s" should be a dict of %s' % (name, el_cls)
                 )
             if attr.has(el_cls):
-                parent[name] = {
-                    k: _deserialize(e, el_cls) for k, e in value.items()
-                }
+                parent[name] = {k: _deserialize(e, el_cls) for k, e in value.items()}
             else:
                 parent[name] = value
         else:
-            raise TypeError('unanticipated field type %s' % field.type)
+            raise TypeError("unanticipated field type %s" % field.type)
     return serializer_cls(**kwargs)
---
+++ liner/serialize_test.py
@@ -0,0 +1,106 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright © 2021 Wrangle Ltd
+
+import typing
+from unittest import TestCase
+
+import attr
+
+from liner.serialize import yaml_load, yaml_dump
+
+
+@attr.s(auto_attribs=True)
+class Person(object):
+    name: str = attr.ib(default=None)
+    birth_date: str = attr.ib(default=None)
+    height: int = attr.ib(default=None)
+    scores: typing.List[int] = attr.ib(default=None)
+
+
+@attr.s(auto_attribs=True)
+class Website(object):
+    url: str = attr.ib(default=None)
+
+
+@attr.s(auto_attribs=True)
+class Employment(object):
+    title: str = attr.ib(default=None)
+
+
+@attr.s(auto_attribs=True)
+class Listing(object):
+    user: Person = attr.ib(default=None)
+    websites: typing.List[Website] = attr.ib(default=None)
+    employments: typing.Dict[str, Employment] = attr.ib(default=None)
+
+
+@attr.s(auto_attribs=True)
+class Animal(object):
+    omnivore: bool = attr.ib(default=None)
+    vertebrate: bool = attr.ib(default=None)
+
+
+class YAMLTestCase(TestCase):
+    def test_loads_simple(self):
+        obj1 = Person(
+            name="John Doe", birth_date="10/02/2000", height=170, scores=[7, 8, 9]
+        )
+        yaml_str = yaml_dump(obj1)
+        self.assertEqual(
+            yaml_str,
+            "\n".join(
+                [
+                    "birthDate: 10/02/2000",
+                    "height: 170",
+                    "name: John Doe",
+                    "scores:",
+                    "- 7",
+                    "- 8",
+                    "- 9",
+                    "",
+                ]
+            ),
+        )
+        obj2 = yaml_load(yaml_str, Person)
+        self.assertEqual(obj1, obj2)
+
+    def test_loads_nested(self):
+        self.maxDiff = None
+        obj1 = Listing(
+            user=Person(name="John Doe", birth_date="10/02/2000", height=170),
+            websites=[
+                Website(url="https://url1"),
+                Website(url="https://url2"),
+            ],
+            employments={
+                "ABC": Employment(title="engineer"),
+                "DEF": Employment(title="head QA"),
+            },
+        )
+        yaml_str = yaml_dump(obj1, indent=4)
+        self.assertEqual(
+            yaml_str,
+            "\n".join(
+                [
+                    "employments:",
+                    "    ABC:",
+                    "        title: engineer",
+                    "    DEF:",
+                    "        title: head QA",
+                    "user:",
+                    "    birthDate: 10/02/2000",
+                    "    height: 170",
+                    "    name: John Doe",
+                    "websites:",
+                    "-   url: https://url1",
+                    "-   url: https://url2",
+                    "",
+                ]
+            ),
+        )
+        obj2 = yaml_load(yaml_str, Listing)
+        self.assertEqual(obj1, obj2)
+
+    def test_loads_ignore_empty(self):
+        obj = yaml_load('{"omnivore": true}', Animal)
+        self.assertEqual(obj, Animal(omnivore=True))
